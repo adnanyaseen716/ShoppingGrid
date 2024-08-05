a94b5ba74ac2897be1752155e6d3b1528e6c0ffb
sami0058 <chsamiullah727@gmail.com>
2024-08-05 20:01:29 +0500

    changes made

--- app/src/main/java/pk/org/cas/shoppinggrid/Splash/SignupForUserActivity.java
+++ app/src/main/java/pk/org/cas/shoppinggrid/Splash/SignupForUserActivity.java
@@ -50,7 +50,7 @@ public class SignupForUserActivity extends AppCompatActivity {
         tvAlreadyAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent LoginIntent = new Intent(SignupForUserActivity.this,SignupForUserActivity.class);
+                Intent LoginIntent = new Intent(SignupForUserActivity.this,LoginForUserActivity.class);
                 startActivity(LoginIntent);
             }
         });
