5d58d5c23f199d7b2cfb4bc9ab1a42a36f15d35f
rafay0000 <itsrafay03@gmail.com>
2024-08-05 22:55:05 +0500

    All fragments Done.

--- app/src/main/java/pk/org/cas/shoppinggrid/Home/HomeFragment.java
+++ app/src/main/java/pk/org/cas/shoppinggrid/Home/HomeFragment.java
@@ -76,8 +76,11 @@ public class HomeFragment extends Fragment {
             @Override
             public void onItemClick(ProductAdapter.ProductViewHolder holder, int position) {
                 Toast.makeText(getContext(), "Clicked on Product", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(), ProductOnclick.class);
+                startActivity(intent);
             }
         });
+
 
         ivNotification.setOnClickListener(new View.OnClickListener() {
             @Override
