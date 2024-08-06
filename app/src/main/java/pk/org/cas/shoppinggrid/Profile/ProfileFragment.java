1ec29f880ee3179dd9134dfd631f8da0f4364072
rafay0000 <itsrafay03@gmail.com>
2024-08-06 17:08:20 +0500

    Updated some activities.

--- app/src/main/java/pk/org/cas/shoppinggrid/Profile/ProfileFragment.java
+++ app/src/main/java/pk/org/cas/shoppinggrid/Profile/ProfileFragment.java
@@ -24,11 +24,6 @@ public class ProfileFragment extends Fragment {
     CardView cvSettings;
     View view;
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
