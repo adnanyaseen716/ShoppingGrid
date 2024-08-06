5c60c4542b25134c55392cd9042e9f8de74616f8
kFasih132 <kfasih132@gmail.com>
2024-08-06 22:39:56 +0500

    cart changes --

--- app/src/main/java/pk/org/cas/shoppinggrid/Cart/CartFragment.java
+++ app/src/main/java/pk/org/cas/shoppinggrid/Cart/CartFragment.java
@@ -6,14 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import pk.org.cas.shoppinggrid.R;
 
 public class CartFragment extends Fragment {
     View view;
-    ImageButton imageButton3;
+    ImageButton imageButton3 ;
+    TextView tv_checkout_now;
+    ConstraintLayout clCheckout;
 
     public CartFragment() {
         // Required empty public constructor
@@ -26,14 +30,25 @@ public class CartFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_cart, container, false);
 
         imageButton3 = view.findViewById(R.id.imageButton3);
+        tv_checkout_now = view.findViewById(R.id.tv_checkout_now);
+        clCheckout = view.findViewById(R.id.clCheckout);
 
-        imageButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), ActivityAddress.class);
-                startActivity(intent);
-            }
+
+
+        clCheckout.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), ActivityAddress.class);
+            startActivity(intent);
         });
+
+
+
+//        imageButton3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getContext(), ActivityAddress.class);
+//                startActivity(intent);
+//            }
+//        });
 
 
         return view;
